595c66a5827e9ba6ce2af3c1e1e5a51fdbfb67b6
Sarah Pennoyer <sarahpennoyer@Sarahs-MacBook-Air.local>
2018-02-07 20:23:26 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -21,29 +21,43 @@ var cards = [
   }
 
 ];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
-  if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-    result = alert("You found a match!");
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
   } else {
-    result = alert("Sorry, try again.");
+  alert("Sorry, try again.");
   }
-}
+};
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+
+var cardId = this.getAttribute('data-id');
+
 console.log("User flipped " + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
 
 console.log(cards[cardId].cardImage);
-cardsInPlay.push(cards[cardId].cardImage);
-
 console.log(cards[cardId].suit);
-cardsInPlay.push(cards[cardId].suit);
 
+this.setAttribute('src', cards[cardId].cardImage);
+if (cardsInPlay.length === 2) {
 checkForMatch();
-
 }
+};
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+
+  var cardElement = document.createElement('img');
+
+  cardElement.setAttribute('src', 'images/back.png');
+  cardElement.setAttribute('data-id', i);
+  cardElement.addEventListener('click', flipCard);
+  document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+createBoard();
